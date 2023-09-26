01277d8c96c075ae922ff183b1d52490176c0c98
Aaron Sequeira <96731649+aaronseq12@users.noreply.github.com>
2023-09-26 13:26:42 +0300

    Update fraud_credit.py

--- fraud_credit.py
+++ fraud_credit.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 from pyod.models.abod import ABOD
 from pyod.models.cblof import CBLOF
 from pyod.models.feature_bagging import FeatureBagging
